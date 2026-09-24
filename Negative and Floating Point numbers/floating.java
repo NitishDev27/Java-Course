8e081397b7e7592d818294709be23caa8df2d815
Nitish <nitishsharma1908@gmail.com>
2026-09-24 13:34:07 +0530

    Add Negative and Floating folder and files

---
+++ Negative and Floating Point numbers/floating.java
@@ -0,0 +1,18 @@
+//Negative and Floating Point numbers;
+
+public class floating {
+    public static void main(String[] args) {
+        // floating numbers (method 1)
+
+        //float f = 0.7f;
+        //System.out.println(f);
+
+        //float f = 8.3534f;
+       // System.out.println(f);
+
+       // floating numbers (method 2) to find out the real value stored in loaction by the compiler
+
+       float f=0.4f;
+       System.out.printf("format:%.20f%n",f);
+    }
+}
